101b5d14f9f46094747233bee97827e0eeed00aa
teosierrar <teosierram@gmail.com>
2025-01-14 18:51:26 +0100

    finished

---
+++ asignment_op.py
@@ -0,0 +1,10 @@
+a = 5
+print(a)
+a+=2
+print(a)
+a-=10
+print(a)
+a*=-2
+print(a)
+a%=4
+print(a)
---
+++ logical_op.py
@@ -0,0 +1,4 @@
+print(2 or 3)
+print(2 and 3)
+print(2 or 2 or 4 or 5)
+print(2 and 2 and 0 and 5)
